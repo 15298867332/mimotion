a8e1df339be1b269c689d78a4bfeddaa3c74a54d
doer <100286678+15298867332@users.noreply.github.com>
2024-10-15 16:32:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def getBeijinTime():
         min_ratio = max(math.ceil((int(hour) / 1) - 6), 0)
         max_ratio = math.ceil(int(hour) / 1)
         min_1 = 520 * min_ratio
-        max_1 = 666 * max_ratio
+        max_1 = 888 * max_ratio
         min_1 = int(K * min_1)
         max_1 = int(K * max_1)
     else:
